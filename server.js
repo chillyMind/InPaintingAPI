d469e47c448af5b9fecfd627033b880b657cbcf7
JeoungWook Lee <visioninmind@naver.com>
2018-05-02 17:20:18 +0900

    python daemon add

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express();
 const multer = require('multer')
 const cmd = require('node-cmd')
+const sleep = require('system-sleep');
+const fs = require('fs')
 const port = 3000;
 
 app.set("view engine",'ejs')
@@ -30,7 +32,9 @@ app.post('/uploadimg',multer({dest:'tmpimage/'}).single('upfile'),function(req,r
   })
 })
 */
-app.post('/upload',multer({dest:'tmpimage/'}).single('upfile'),function(req,res){
+
+// not python daemon
+app.post('/uploadND',multer({dest:'tmpimage/'}).single('upfile'),function(req,res){
   console.log('filename :'+req.file.filename + " is uploaded");
   cmd.get(
     'python netmodels/noneSquare_singletest.py --testimg tmpimage/'+req.file.filename,
@@ -39,7 +43,7 @@ app.post('/upload',multer({dest:'tmpimage/'}).single('upfile'),function(req,res)
         console.log(err)
         res.send("error occured" + err)
       }else{
-        console.log("coloring has been doned")
+        console.log("coloring has been done")
         const filename = req.file.filename + ".png"
         const options = {
           root : __dirname + '/public/result/tmpimage/',
@@ -58,6 +62,42 @@ app.post('/upload',multer({dest:'tmpimage/'}).single('upfile'),function(req,res)
         })
       }
   })
+})
+app.post('/upload',multer({dest:'tmpimage/'}).single('upfile'),function(req,res){
+  console.log('filename :'+req.file.filename + " is uploaded");
+  var i = 0;
+  var outputfilename = req.file.filename+".png";
+  var flag = false;
+  while(i < 60){
+    if(flag) break;
+    fs.readdir('./public/result', (err,files) => {
+      files.forEach(file => {
+        if ( outputfilename == file){
+          console.log("coloring has been done")
+          const filename = outputfilename
+          const options = {
+            root : __dirname + '/public/result/',
+            dotfiles:'deny',
+            headers:{
+              'x-timestamp' : Date.now(),
+              'x-sent' :true
+            }
+          }
+          res.sendFile(filename,options,function(err){
+            if(err){
+              next(err);
+            }else{
+              console.log('Sent',filename);
+            }
+          })
+          flag = true;
+        }
+      })
+    })
+    sleep(1000)
+    i++
+  }
+
 })
 
 
